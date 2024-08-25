437896ea22cf1b97b2eb17c6ff63338fe97e2873
GPrensLuna <prens.giova12@gmail.com>
2024-08-25 15:17:48 -0500

    agregando buton de git download y libro

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 
 const FilteredCardList = dynamic(() => import('@/components/FilteredCardList'), {
   ssr: false,
@@ -11,7 +12,20 @@ const HomePage = () => {
         <h1 className="text-4xl font-extrabold text-navyBlue dark:text-lightGray text-center mb-8">
           Git Commands and Code Snippets
         </h1>
-
+        <section className='w-full grid grid-cols-2 items-center justify-center gap-5 '>
+          <Link
+            className=" bg-lightBlue text-white dark:text-darkGray p-1 rounded-md shadow-sm hover:bg-skyBlue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lightBlue transition-colors text-sm font-bold uppercase text-center px-8"
+            href={"https://git-scm.com/download/win"}
+            target='_blank'>
+            Descargar git
+          </Link>
+          <Link
+            className=" bg-lightBlue text-white dark:text-darkGray p-1 rounded-md shadow-sm hover:bg-skyBlue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lightBlue transition-colors text-sm font-bold uppercase text-center px-8"
+            href={"https://git-scm.com/book/en/v2"}
+            target='_blank'>
+            Descargar Libro de git
+          </Link>
+        </section>
         <FilteredCardList />
       </div>
     </main>
